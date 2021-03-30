c7073c422228590663ebc0b3a445d72d0fa307af
Jan Zak <zj@zakjan.cz>
2021-03-30 09:49:42 +0200

    Use @tailwindcss/jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,11 @@ module.exports = {
   purge: [
     './src/index.html',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      transitionProperty: {
-       'margin': 'margin',
-      },
-    },
     colors: {
       orange: colors.orange,
       gray: colors.warmGray,
       white: colors.white,
-    },
-  },
-  variants: {
-    extend: {
-      margin: ['hover'],
     },
   },
   plugins: [
